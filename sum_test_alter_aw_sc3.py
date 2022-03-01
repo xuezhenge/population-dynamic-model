f65118f50569cddeae559eba86555c837f88edb6
xuezhenge <43679982+xuezhenge@users.noreply.github.com>
2022-03-01 14:09:20 -0500

    Update sum_test_alter_aw_sc3.py

--- sum_test_alter_aw_sc3.py
+++ sum_test_alter_aw_sc3.py
@@ -36,7 +36,7 @@ def get_date(a,w):
         t = symbols('t')
         Temp = Eq(-a*sympy.cos(2*sympy.pi*t/365) + w, 10)
         sol = solve(Temp)
-        date = int(sol[1]) + 1
+        date = int(sol[1])
         if date > 365:
             date = 365
     return date
