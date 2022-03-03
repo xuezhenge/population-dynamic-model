aa60f78c02dfcf827b9797cb6065ed73f6e27e5f
xuezhenge <43679982+xuezhenge@users.noreply.github.com>
2022-03-03 12:50:57 -0500

    Update R03_create_folders.py

--- R03_create_folders.py
+++ R03_create_folders.py
@@ -14,7 +14,7 @@ case = args.case
 alter = args.alter
 
 def main(args):
-    fold_dir = f"./outputs/exports_case{case}_{alter}"
+    fold_dir = f"../outputs/exports_case{case}_{alter}"
     scenarios = ['eco']
     for scenario in scenarios:
         for folder_name in ["data", "plotAnp", "plotAp", "plotL", "plotAL"]:
