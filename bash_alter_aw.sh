6d59d84dd54c60c015e2e0516fa56f34ac4a6fc7
xuezhenge <43679982+xuezhenge@users.noreply.github.com>
2022-03-03 23:39:39 -0500

    Update bash_alter_aw.sh

--- bash_alter_aw.sh
+++ bash_alter_aw.sh
@@ -1,25 +1,9 @@
-for i in 2080
+
+for i in aw00 aw04 aw08 aw40 aw44 aw48 aw-40 aw-44 aw-48
 do
     for j in 0 1 2 3 4 5 6 7 8
     do
-        for h in aw00 aw02 aw04 aw-40 aw40 aw42 aw44 aw-42 aw-44
-        #for h in aw00
-        do
-        python sum_test_alter_aw_sc3.py --year=$i --case=$j --alter=$h
-        done
+    python R04sum_outputs.py  --alter=$i --case=$j
     done
 done
-
-
-# for i in 2080
-# do
-#     for j in 0 1 2 3 4 5 6 7 8
-#     do
-#         #for h in aw00 aw02 aw04 aw-40 aw40 aw42 aw44 aw-42 aw-44
-#         for h in aw00
-#         do
-#         python sum_test_alter_aw_sc2.py --year=$i --case=$j --alter=$h
-#         done
-#     done
-# done
 
