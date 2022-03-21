2486de57044141acca3a6ad2440b455e23a9a2ec
xuezhenge <43679982+xuezhenge@users.noreply.github.com>
2022-03-21 19:45:43 -0400

    Update R02_generate_jobs.py

--- R02_generate_jobs.py
+++ R02_generate_jobs.py
@@ -28,7 +28,7 @@ def main(args):
     num_cores = 24
     num_idxs = 1160
     num_jobs = num_idxs // num_idxs_per_job + 1
-    script_name = f"model3_cc.py"
+    script_name = f"model3_30years.py"
 
     # starter for job scripts
     header = "#!/bin/bash" + "\n"
