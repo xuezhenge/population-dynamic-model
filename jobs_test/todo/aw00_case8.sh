05646983a1122e8ab9d64114a39740f336661cfa
xuezhenge <43679982+xuezhenge@users.noreply.github.com>
2022-04-05 13:31:51 -0400

    Update aw00_case8.sh

--- jobs_test/todo/aw00_case8.sh
+++ jobs_test/todo/aw00_case8.sh
@@ -2,4 +2,4 @@
 eval "$(conda shell.bash hook)"
 conda activate
 cd /home/$USER/scratch/eco_model/population-dynamic-model
-python sum_outputs_50years3.py --alter=aw00 --case=9
+python sum_outputs_50years3.py --alter=aw00 --case=8
