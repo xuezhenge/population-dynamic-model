59d6a95d517201e40dbd6e9ee54871b20ba90c14
xuezhenge <43679982+xuezhenge@users.noreply.github.com>
2022-04-11 15:11:57 -0400

    Update and rename aw00_case0.sh to sum_aw.sh

--- jobs_test/todo/sum_aw.sh
+++ jobs_test/todo/sum_aw.sh
@@ -2,4 +2,4 @@
 eval "$(conda shell.bash hook)"
 conda activate
 cd /home/$USER/scratch/eco_model/population-dynamic-model
-python sum_outputs_50years3.py --alter=aw00 --case=0
+python sum_outputs_50years3_np.py
