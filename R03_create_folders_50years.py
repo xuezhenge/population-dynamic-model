701467af3ce3bc879fd8365868cff1bee8fbee2d
xuezhenge <43679982+xuezhenge@users.noreply.github.com>
2022-04-12 10:46:21 -0400

    Update R03_create_folders_50years.py

--- R03_create_folders_50years.py
+++ R03_create_folders_50years.py
@@ -14,7 +14,7 @@ case = args.case
 alter = args.alter
 
 def main(args):
-    fold_dir = f"../outputs/exports_case{case}_{alter}_50years_np"
+    fold_dir = f"../outputs/exports_case{case}_{alter}_30years_np"
     scenarios = ['eco']
     for scenario in scenarios:
         for folder_name in ["data", "plotAnp", "plotAp", "plotL", "plotAL"]:
