d556806a4502156cbdf90b767d7288455b53cd72
xuezhenge <43679982+xuezhenge@users.noreply.github.com>
2022-04-12 09:00:54 -0400

    Update batch_sum.py

--- batch_sum.py
+++ batch_sum.py
@@ -153,9 +153,8 @@ def out_csv(i,idxs):
     #import pdb;pdb.set_trace()
     writer_csv(rows,filename = fn_AAP)
 
-cases = [3,6,7]
+cases = [0,1,2,3,4,5,6,7]
 alters = ['aw00','aw04','aw08','aw-40','aw-44','aw-48','aw40','aw44','aw48']
-alters = ['aw-40']
 
 idxs = []
 for alter in alters:
