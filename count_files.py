703a05d40108d4a9b550bdf48574989ee5d0b7aa
xuezhenge <43679982+xuezhenge@users.noreply.github.com>
2022-04-15 10:47:26 -0400

    Update count_files.py

--- count_files.py
+++ count_files.py
@@ -8,7 +8,7 @@ cases = [0,1,2,3,4,5,6,7,8]
 alters = ['aw00','aw04','aw08','aw-40','aw-44','aw-48','aw40','aw44','aw48']
 for alter in alters:
     for case in cases:
-        data_dir = f'../outputs/exports_case{case}_{alter}_50years/eco_data'
+        data_dir = f'../outputs/exports_case{case}_{alter}_30years_np/eco_data'
         files = os.listdir(data_dir)
         no_files = len(files)
         print(f'{alter} -- case{case}: {no_files} files')
