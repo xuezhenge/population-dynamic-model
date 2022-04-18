41ade3564b9d9620bacb893278899c01d9bf1ac5
xuezhenge <43679982+xuezhenge@users.noreply.github.com>
2022-04-18 14:41:16 -0400

    Update A0L0_counts.py

--- A0L0_counts.py
+++ A0L0_counts.py
@@ -17,7 +17,6 @@ cases = [0,1,2,3,4,5,6,7,8]
 for case in cases:
     df = pd.read_csv(f'../outputs/sum_csv/AAPs_sum_{alter}_50years/AAPs_case{case}.csv')
     df = df[df["Class"] == 'A0L0']
-    df = df[args.start_idx:args.end_idx]
     a = np.array(df.a)
     len = len(a)
     print(alter, case, a)
