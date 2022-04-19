1d86b606f3d7af0fc27848b894fa39e50cf9cbc4
xuezhenge <43679982+xuezhenge@users.noreply.github.com>
2022-04-19 12:10:14 -0400

    Update job_sum_np2.sh

--- job_sum_np2.sh
+++ job_sum_np2.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+#SBATCH --account=def-encaenia-ab   # replace this with your own account
+#SBATCH --mem=10G     # memory; default unit is megabytes
+#SBATCH --time=3:00:00             # time (DD-HH:MM)
+python R04batch_sum_np2.py
 
