1b649786d5a0d17f2dca5c4be8610d0d03ae097d
xuezhenge <43679982+xuezhenge@users.noreply.github.com>
2022-04-20 17:09:38 -0400

    Update R04batch_sum_p.py

--- R04batch_sum_p.py
+++ R04batch_sum_p.py
@@ -156,9 +156,9 @@ def out_csv(i,idxs):
     #import pdb;pdb.set_trace()
     writer_csv(rows,filename = fn_AAP)
 
-cases = [0,1,2,3,4,5,6,7,8]
+cases = [5]
 alters = ['aw00','aw04','aw08','aw-40','aw-44','aw-48','aw40','aw44','aw48']
-alters = ['aw04','aw48']
+alters = ['aw-40']
 
 idxs = []
 for alter in alters:
