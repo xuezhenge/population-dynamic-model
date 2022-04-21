b02eeceacfa429ba6e7ebb50ab42eb131b4c5c4d
xuezhenge <43679982+xuezhenge@users.noreply.github.com>
2022-04-21 00:48:06 -0400

    Update R02_generate_jobs_np.py

--- R02_generate_jobs_np.py
+++ R02_generate_jobs_np.py
@@ -23,7 +23,7 @@ def main(args):
     if not os.path.exists(jobs_dir):
         os.makedirs(jobs_dir)
 
-    cases = [7,8]
+    cases = [0,1,3,4,5,6]
     # num_jobs
     num_cores = 24
     num_idxs = 1160
