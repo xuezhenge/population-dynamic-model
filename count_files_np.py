0f449313b0e1a163e48f4781010519748c308a3e
xuezhenge <43679982+xuezhenge@users.noreply.github.com>
2022-04-22 14:28:15 -0400

    Update count_files_np.py

--- count_files_np.py
+++ count_files_np.py
@@ -6,6 +6,7 @@ import os
 
 cases = [0,1,2,3,4,5,6,7,8]
 alters = ['aw00','aw04','aw08','aw-40','aw-44','aw-48','aw40','aw44','aw48']
+alters = ['aw08','aw-40','aw-44','aw-48','aw44','aw48']
 for alter in alters:
     for case in cases:
         data_dir = f'../outputs/exports_case{case}_{alter}_30years_np/eco_data'
