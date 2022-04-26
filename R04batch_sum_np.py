32b0ceac745fe6960f2d6b7b4471ae610180b4db
xuezhenge <43679982+xuezhenge@users.noreply.github.com>
2022-04-26 10:25:46 -0400

    Update R04batch_sum_np.py

--- R04batch_sum_np.py
+++ R04batch_sum_np.py
@@ -88,7 +88,7 @@ for alter in alters:
     for case in cases:
         idx = [alter,case]
         idxs += [idx]
-idxs = [['aw48',6],['aw48',7],['aw48',8],['aw44',4],['aw48',1]]
+idxs = [['aw48',1],['aw44',4]]
 num_idxs = len(idxs)
 num_cores = 1
 for i in np.arange(num_idxs) :
